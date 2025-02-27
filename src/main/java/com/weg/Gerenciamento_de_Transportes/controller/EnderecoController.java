94aa6fd866f7db85e59e6633dc5124ae1cfd5919
Pedro-Panstein <phfarof@gmail.com>
2025-02-27 16:59:58 -0300

    Endereco controller created

---
+++ src/main/java/com/weg/Gerenciamento_de_Transportes/controller/EnderecoController.java
@@ -0,0 +1,29 @@
+package com.weg.Gerenciamento_de_Transportes.controller;
+
+import com.weg.Gerenciamento_de_Transportes.model.Endereco;
+import com.weg.Gerenciamento_de_Transportes.model.dtos.request.EnderecoRequestDTO;
+import com.weg.Gerenciamento_de_Transportes.model.dtos.response.EnderecoResponseDTO;
+import com.weg.Gerenciamento_de_Transportes.service.EnderecoService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("endereco")
+@AllArgsConstructor
+@NoArgsConstructor
+public class EnderecoController {
+    private EnderecoService service;
+
+    @PostMapping("/adicionar")
+    public ResponseEntity<EnderecoResponseDTO> criarEndereco(@RequestBody @Validated EnderecoRequestDTO enderecoRequestDTO) {
+        EnderecoResponseDTO novoEndereco = service.criarEndereco(enderecoRequestDTO);
+        return new ResponseEntity<>(novoEndereco, HttpStatus.CREATED);
+    }
+}
--- src/main/java/com/weg/Gerenciamento_de_Transportes/service/EnderecoService.java
+++ src/main/java/com/weg/Gerenciamento_de_Transportes/service/EnderecoService.java
@@ -5,6 +5,8 @@ import com.weg.Gerenciamento_de_Transportes.model.dtos.request.EnderecoRequestDT
 import com.weg.Gerenciamento_de_Transportes.model.dtos.response.EnderecoResponseDTO;
 import com.weg.Gerenciamento_de_Transportes.repository.EnderecoRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,8 +14,28 @@ import org.springframework.stereotype.Service;
 public class EnderecoService {
     private EnderecoRepository repository;
 
-    public EnderecoResponseDTO criarMotorista(EnderecoRequestDTO enderecoRequestDTO) {
+    public EnderecoResponseDTO criarEndereco(EnderecoRequestDTO enderecoRequestDTO) {
         Endereco endereco = enderecoRequestDTO.convertToEntity();
         return repository.save(endereco).convertToDto();
+    }
+
+    public EnderecoResponseDTO editarEndereco(Long id, EnderecoRequestDTO enderecoRequestDTO) {
+        Endereco novoEndereco = enderecoRequestDTO.convertToEntity();
+        novoEndereco.setId(id);
+        return repository.save(novoEndereco).convertToDto();
+    }
+
+    public EnderecoResponseDTO buscarEnderecoPorId(Long id) {
+        Endereco endereco = repository.findById(id).orElseThrow();
+        return endereco.convertToDto();
+    }
+
+    public Page<EnderecoResponseDTO> listarTodosEnderecos(Pageable pageable) {
+        return repository.findAll(pageable).map(Endereco::convertToDto);
+    }
+
+    public String deletarEndereco(Long id) {
+        repository.deleteById(id);
+        return "Endereço deletado com sucesso";
     }
 }
